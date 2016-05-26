70781b0f4ae91c57adbd5a80f206c38434463c58
M. MUHSIN <munashif.muhsin@gmail.com>
2016-05-26 15:45:58 +0530

    added DB authentication
    added DB authentication

--- database.js
+++ database.js
@@ -2,5 +2,5 @@
  * Created by Muhsin on 26/05/2016.
  */
 var neo4j = require('neo4j');
-var db = new neo4j.GraphDatabase('http://localhost:7474');
+var db = new neo4j.GraphDatabase('http://neo4j:neo4jpass@localhost:7474');
 module.exports = db;
